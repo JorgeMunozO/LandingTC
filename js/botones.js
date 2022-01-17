14f089bcf9b54841456e9c540dae9e8f99a6dfe4
Jorge <86856846+JorgeMunozO@users.noreply.github.com>
2022-01-17 14:22:22 -0500

    mas arreglos

--- js/botones.js
+++ js/botones.js
@@ -15,11 +15,15 @@ const tablaSi="<table class='table1 positionTableSi' ><thead><tr class=''><th sc
 "<tr><th><div class=' text-center line-normal4ta'><p class='text-font500 text-xx'><span class=' text-azulOsc'>Realiza el mayor número de transacciones </span><span class='text-col-negro'>durante <br>la vigencia de la campaña. Ganan los 25 clientes <br>con mayor número de transacciones.</span></p></div></th>"+
 "<th><div class='text-center line-normal4ta'><p class='text-fontM text-4taSection'><span class=' text-azulOsc'>$200.000*</span></p></div></th></tr></tbody></table>"
 
+
+
 const tablaNo="<table class='table1 ' style='margin: 0 auto;'><thead><tr class=''><th scope=''><div class=' text-center  line-normal4ta'><p class='text-fontM text-4taSection'><span class='text-fontM text-azulOsc'>Misión</span></p></div></th><th scope=''><div class=' text-center  line-normal4ta'><p class='text-fontM text-4taSection'><span class='text-fontM text-azulOsc'>Premio</span></p></div></th></tr></thead>"+
 "<tbody><tr class=''><th ><div class=' text-center line-normal4ta'><p class='text-font500 text-xx'><span class='text-azulOsc'>Reclama tu Tarjeta Débito en nuestras oficinas <br>y úsala mínimo 5 veces. </span><span class='text-col-negro'>Ganan los primeros 200 <br> clientes en cumplir la meta.</span></p></div></th>"+
 "<th ><div class=' text-center line-normal4ta'><p class='text-fontM text-4taSection'><span class=' text-azulOsc'>$50.000*</span></p></div></th></tr>"+
-"<tr><th><div class=' text-center line-normal4ta'><p class='text-font500 text-xx'><span class=' text-azulOsc'>Usa tu tarjeta por lo menos una vez almes.</span></p></div></th>"+
-"<th><div class=' text-center line-normal4ta'><p class='text-fontM text-4taSection'><span class=' text-azulOsc'>Cuota de Manejo Gratis</span><br><span class='text-col-negro text-font500 text-xx'>por 3 meses (enero, febreroy marzo)</p></div></th></tr>"+
+
+"<tr><th><div class=' text-center line-normal4ta'><p class='text-font500 text-xx'><span class=' text-azulOsc'>Usa tu tarjeta por lo menos una vez al mes.</span></p></div></th>"+
+"<th><div class=' text-center line-normal4ta'><p class='text-fontM text-4taSection'><span class=' text-azulOsc'>Cuota de Manejo Gratis</span><br><span class='text-col-negro text-font500 text-xx'>por 3 meses (enero, febrero y marzo)</p></div></th></tr>"+
+
 "<tr><th><div class=' text-center line-normal4ta'><p class='text-font500 text-xx'><span class=' text-azulOsc'>Realiza el mayor número de transacciones </span><span class='text-col-negro'>durante <br>la vigencia de la campaña (Mínimo 8 <br> transacciones para participar). Ganan los 25 clientes <br>con mayor número de transacciones.</span></p></div></th>"+
 "<th><div class='text-center line-normal4ta'><p class='text-fontM text-4taSection'><span class=' text-azulOsc'>$200.000*</span></p></div></th></tr></tbody></table>"
 
